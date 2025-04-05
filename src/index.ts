9ae09bb82afa91b565adeda62df980917842d3fc
Jorge Cruz <1081844+jcruz103@users.noreply.github.com>
2025-04-05 13:36:34 -0500

    init

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import express, { Request, Response } from "express";
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { SSEServerTransport } from "@modelcontextprotocol/sdk/server/sse.js";
+
+const server = new McpServer({
+    name: "weather-server",
+    version: "1.0.0"
+});
