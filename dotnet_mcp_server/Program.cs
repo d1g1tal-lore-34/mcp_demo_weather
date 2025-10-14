cbb50529b7a3197533a8752d5fcea429f13c3b63
Jorge Cruz <1081844+d1g1tal-lore-34@users.noreply.github.com>
2025-10-13 22:09:17 -0500

    working mcp server with azure entra

--- dotnet_mcp_server/Program.cs
+++ dotnet_mcp_server/Program.cs
@@ -1,6 +1,35 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Identity.Web;
+using ModelContextProtocol.AspNetCore.Authentication;
+using ModelContextProtocol.Authentication;
+using dotenv.net;
+DotEnv.Load();
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = McpAuthenticationDefaults.AuthenticationScheme;
+}).AddMcp(option =>
+{
+    var identityOptions = builder
+            .Configuration.GetSection("AzureAd")
+            .Get<MicrosoftIdentityOptions>()!;
+
+    option.ResourceMetadata = new ProtectedResourceMetadata
+    {
+        Resource = GetMcpServerUrl(),
+        AuthorizationServers = [GetAuthorizationServerUrl(identityOptions)],
+        ScopesSupported = [$"api://{identityOptions.ClientId}/MCP.All"],
+    };
+})
+.AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddAuthorization();
+builder.Services.AddMcpServer().WithToolsFromAssembly().WithHttpTransport();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -18,8 +47,13 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
+app.MapMcp().RequireAuthorization();
 
-app.MapControllers();
+// app.MapControllers();
 
 app.Run();
+
+static Uri GetAuthorizationServerUrl(MicrosoftIdentityOptions o) => new($"{o.Instance?.TrimEnd('/')}/{o.TenantId}/v2.0");
+Uri GetMcpServerUrl() => builder.Configuration.GetValue<Uri>("McpServerUrl");
